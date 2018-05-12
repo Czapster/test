2122e4c3fe1c53955c48c6264db1e54b646ecffe
Czapster <anna.czapiewska@gmail.com>
2018-05-12 13:16:28 +0100

    my first repository

---
+++ guczini.py
@@ -0,0 +1,12 @@
+# this will print your name
+print ('Hello')
+print ('What is your name?')
+myName= input()
+print ('Nice to meet you' + myName)
+print('Yhe lenght of your name is:')
+print (len(myName))
+print ('What is your age?')
+yourAge = input()
+print ('So you will be '+ str(int(yourAge) +1) + 'in a year')
+      
+ 
